d21c8ddaa04828f3e81b63ffcd8b34cd41bd5dbb
BintangCoder <bintangilham120@gmail.com>
2026-03-03 14:30:56 +0700

    DONE

--- pilem/lib/main.dart
+++ pilem/lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:pilem/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+      };
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +20,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: AppScrollBehavior(),
+      title: "Pilem",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
     );
   }
 }
