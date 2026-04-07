5791a0bcc939751b477c44728f4ee2c84f1496bb
BintangCoder <bintangilham120@gmail.com>
2026-04-07 12:12:56 +0700

    Done Latihan UTS

--- mdp_gold/lib/main.dart
+++ mdp_gold/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mdp_gold/screens/price_list_screen.dart';
 import 'firebase_options.dart';
 import 'package:mdp_gold/screens/price_list_screen.dart';
 
 void main() async {
+  // Pastikan Flutter binding sudah diinisialisasi sebelum menggunakan Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inisialisasi Firebase dengan opsi yang sesuai untuk platform saat ini
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
